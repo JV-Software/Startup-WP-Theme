942d391228158e6c60ab153b999c2545cff013d3
Javier Villanueva <javiervd@gmail.com>
2012-06-03 00:52:00 -0430

    Add viewport meta tag for better mobile support

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
         <meta charset="<?php bloginfo('charset'); ?>" />
         <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
         <link rel="profile" href="http://gmpg.org/xfn/11" />
+        <meta name="viewport" content="width=device-width" />
         <title>
         <?php
             if ( is_home() || is_front_page() ) { bloginfo('name'); echo ' | '; bloginfo('description'); }

992dc2e36769bc2b929726e85ed7dd82d204b21e
Javier Villanueva <javiervd@gmail.com>
2012-11-18 11:39:32 -0430

    Add selectivizr to exclude watch list

--- src/grunt.js
+++ src/grunt.js
@@ -24,7 +24,7 @@ module.exports = function(grunt) {
     },
     concat: {
       dist: {
-        src: ['<banner:meta.banner>', '../js/libs/!(modernizr).js', '../js/script.js'],
+        src: ['<banner:meta.banner>', '../js/libs/!(modernizr|selectivizr).js', '../js/script.js'],
         dest: '../js/script.min.js'
       }
     },

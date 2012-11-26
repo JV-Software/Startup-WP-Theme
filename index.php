1dc6a94ad76d0c518a2d7ff7020e724401aa116d
Javier Villanueva <javiervd@gmail.com>
2012-11-26 15:37:51 -0430

    Remove a level of identation on index

--- index.php
+++ index.php
@@ -1,24 +1,24 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?> class="no-js">
-    <head>
-        <meta charset="<?php bloginfo('charset'); ?>" />
-        <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-        <link rel="profile" href="http://gmpg.org/xfn/11" />
-        <meta name="viewport" content="width=device-width" />
-        <title>
-        <?php
-            if ( is_home() || is_front_page() ) { bloginfo('name'); echo ' | '; bloginfo('description'); }
-            elseif ( is_search() ) { bloginfo('name'); echo ' | Results for: ' . wp_specialchars($s); }
-            elseif ( is_404() ) { bloginfo('name'); echo ' | Not found'; }
-            else { bloginfo('name'); wp_title(' | '); }
-        ?>
-        </title>
-        <?php wp_head(); ?>
-        <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>"  />
-    </head>
-    <body <?php body_class(); ?>>
-		
-	
-        <?php wp_footer(); ?>
-	</body>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>" />
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+    <link rel="profile" href="http://gmpg.org/xfn/11" />
+    <meta name="viewport" content="width=device-width" />
+    <title>
+    <?php
+        if ( is_home() || is_front_page() ) { bloginfo('name'); echo ' | '; bloginfo('description'); }
+        elseif ( is_search() ) { bloginfo('name'); echo ' | Results for: ' . wp_specialchars($s); }
+        elseif ( is_404() ) { bloginfo('name'); echo ' | Not found'; }
+        else { bloginfo('name'); wp_title(' | '); }
+    ?>
+    </title>
+    <?php wp_head(); ?>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>"  />
+</head>
+<body <?php body_class(); ?>>
+    
+
+    <?php wp_footer(); ?>
+</body>
 </html>

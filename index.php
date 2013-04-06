82502d09d2438cb3f26e7450d28d71a502658ac7
Javier Villanueva <javiervd@gmail.com>
2013-04-06 12:24:41 -0430

    Remove device width constraint

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
     <meta charset="<?php bloginfo('charset'); ?>" />
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
     <link rel="profile" href="http://gmpg.org/xfn/11" />
-    <meta name="viewport" content="width=device-width" />
     <title>
     <?php
         if ( is_home() || is_front_page() ) { bloginfo('name'); echo ' | '; bloginfo('description'); }
@@ -17,7 +16,7 @@
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>"  />
 </head>
 <body <?php body_class(); ?>>
-    
+
 
     <?php wp_footer(); ?>
 </body>

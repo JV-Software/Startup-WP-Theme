3cccaa83e3e1c1a2761b0286703fcbcec7cbf4a2
Javier Villanueva <javiervd@gmail.com>
2013-04-06 11:07:37 -0430

    Remove un-necessary whitespace

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?> class="no-js">
 <head>
     <meta charset="<?php bloginfo('charset'); ?>" />
-    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
     <link rel="profile" href="http://gmpg.org/xfn/11" />
     <meta name="viewport" content="width=device-width" />
     <title>

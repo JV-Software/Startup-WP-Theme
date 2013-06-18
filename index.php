56e33f0b368b687cc5ab5ef713da996fdccac426
Javier Villanueva <javiervd@gmail.com>
2013-06-18 10:12:59 -0430

    Add viewport meta tag by default

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html <?php language_attributes(); ?> class="no-js">
 <head>
     <meta charset="<?php bloginfo('charset'); ?>" />
+    <meta name="viewport" content="width=device-width" />
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
     <link rel="profile" href="http://gmpg.org/xfn/11" />
     <title>
